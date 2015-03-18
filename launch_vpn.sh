f6c4c4bb5bc4e1e192ad24ca50d9052e820beb35
chamunks <nixium.net@gmail.com>
2015-03-18 19:46:14 -0400

    added extremely basic openvpn configuration, there are more secure methods of doing this but this is the fastest.  You can look into how its all made if you're creative enough.  Once you launch this with docker you just have to navigate to https://your-server-ip-or-domain.com:8080 (or whatever port you launched to obtain the openvpn configuration.

---
+++ launch_vpn.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+##########################
+# Instant OpenVPN Server #
+# Just add Docker        #
+##########################
+CID=$(docker run -d --privileged -p 1194:1194/udp -p 443:443/tcp jpetazzo/openvpn)
+docker run -t -i -p 8080:8080 --volumes-from $CID jpetazzo/openvpn serveconfig
